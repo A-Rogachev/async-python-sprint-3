aaf04862278220508dfd9301ea28b46bb77e6757
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-10-08 21:02:43 +0400

    first structure

--- client.py
+++ client.py
@@ -1,9 +1,75 @@
-# import asyncio
+import asyncio
+
+import os
+import sys
+
+def clear_console():
+    """
+    Очистка окна терминала клиента после введения логина.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    def __init__(self, server_host, server_port):
+        """
+        Инициализация объекта класса Клиент.
+        """
+        self.server_host: str = server_host
+        self.server_port: int = server_port
+        self.client_id: int = 0
+        self.chat_messages = []
+        self.messages_received = []
 
-    def send(self, message=""):
-        pass
+    async def send_message(self, writer):
+        """
+        Отправка сообщения.
+        """
+        while True:
+            message = await self.get_user_input('Enter a message (or "exit" to exit): ')
+            writer.write((message + '\n').encode())
+            await writer.drain()
+
+            if message == 'exit':
+                break
+
+    async def handle_message(self, reader):
+        """
+        Обработка входящих сообщений.
+        """
+        while True:
+            data = await reader.readline()
+            if not data:
+                break
+            message = data.decode().strip()
+            print(message)  # Print the received message
+
+    async def get_user_input(self, prompt):
+        """
+        Получение ввода пользователя.
+        """
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(None, input, prompt)
+
+    async def start(self):
+        """
+        Запуск клиента, установка связи с сервером.
+        """
+        reader, writer = await asyncio.open_connection(self.server_host, self.server_port)
+
+        nickname: str = await self.get_user_input("Enter your nickname: ")
+        writer.write(nickname.encode() + b'\n')
+        await writer.drain()
+
+        clear_console()
+
+        send_task = asyncio.create_task(self.send_message(writer))
+        receive_task = asyncio.create_task(self.handle_message(reader))
+        await asyncio.gather(send_task, receive_task)
+
+        writer.close()
+
+
+if __name__ == '__main__':
+    client = Client(server_host='127.0.0.1', server_port=8000)
+    asyncio.run(client.start())
--- server.py
+++ server.py
@@ -1,9 +1,76 @@
-# import asyncio
+import logging
+from typing import Any
+import asyncio
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
-        pass
+    """
+    Асинхронный мессенджер (сервер).
+    """
+    def __init__(self, host="127.0.0.1", port=8000, max_chat_messages=20):
+        """
+        Инициализация экземпляра класса Сервер.
+        """
+        self.host: str = host
+        self.port: int = port
+        self.max_chat_messages: int = max_chat_messages
 
-    def listen(self):
-        pass
+        self.chat_messages = []
+        self.connected_clients: dict[str, Any] = {}
+
+    async def client_connected(self, reader, writer):
+        """
+        Обработка подключения нового клиента к серверу.
+        """
+        address = writer.get_extra_info('peername')
+
+        user_nickname: str = (await reader.readline()).decode().strip()
+        self.connected_clients[user_nickname] = writer
+        logger.info(f'---User {user_nickname} is connected--- (ip={address[0]}, port={address[1]})')
+
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            else:
+                message = data.decode().strip()
+                
+                if message.startswith('@'):
+                    tokens = message[1:].split(' ', 1)
+                    if len(tokens) == 2:
+                        recipient, private_message = tokens
+                        if recipient in self.connected_clients:
+                            recipient_writer = self.connected_clients[recipient]
+                            recipient_writer.write(f'Private message from {user_nickname}: {private_message}\n'.encode())
+                            await recipient_writer.drain()
+                        else:
+                            writer.write(f'User {recipient} is not connected\n'.encode())
+                            await writer.drain()
+                else:
+                    logger.info(f'{user_nickname}: {message}')
+                    self.chat_messages.append(f'{user_nickname}: {message}')
+                    for client_nickname, client_writer in self.connected_clients.items():
+                        if client_writer != writer:
+                            client_writer.write(f'{user_nickname}: {message}\n'.encode())
+                            await client_writer.drain()
+
+        logger.info(f'---User {user_nickname} disconnected---')
+        writer.close()
+        del self.connected_clients[user_nickname]
+
+    async def listen(self):
+        """
+        Запуск сервера.
+        """
+        srv = await asyncio.start_server(
+            self.client_connected, self.host, self.port)
+        async with srv:
+            logger.info('Server started on %s:%s', self.host, self.port)
+            await srv.serve_forever()
+
+if __name__ == '__main__':
+    server = Server(host='127.0.0.1', port=8000, max_chat_messages=3)
+    asyncio.run(server.listen())
