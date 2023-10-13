493630617b8a23fb6caf6f487975a2f8af0d456d
Aleksandr Rogachev <alxrogachev@gmail.com>
2023-10-13 23:20:56 +0400

    change str max len flake8

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ class Server:
         self,
         host="127.0.0.1",
         port: int = 8000,
-        max_chat_messages: int = 20,
+        max_chat_messages: int = 10,
         message_ttl: int = 10,
     ):
         """
@@ -58,7 +58,7 @@ class Server:
         )
 
         if self.chat_messages:
-            for message in self.chat_messages:
+            for message in self.chat_messages[-self.max_chat_messages:]:
                 writer.write(f'History!{message[1]}\n'.encode())
                 await writer.drain()
 
@@ -66,7 +66,6 @@ class Server:
             data = await reader.read(1024)
             if not data:
                 break
-
             message: str = data.decode().strip()
             if message.startswith('@'):
                 await self.handle_command(message, user_nickname, writer)
@@ -76,7 +75,7 @@ class Server:
                         message_date := datetime.datetime.now(),
                         message_text := (
                             f'({message_date.strftime("%d.%m.%y %H:%M:%S")}) '
-                            f' {user_nickname}: {message}'
+                            f'{user_nickname}: {message}'
                         )
                     )
                 )
@@ -99,40 +98,56 @@ class Server:
         Обработка команд мессенджера.
         """
         if message == '@help':
-            writer.write(self.help_message.encode())
-            await writer.drain()
+            await self.send_help_message(writer)
         elif message.startswith('@claim'):
-            # Для обработки жалоб на клиента.
-            ...
-        else:
             tokens = message[1:].split(' ', 1)
             if len(tokens) == 2:
-                recipient, private_message = tokens
+                recipient = tokens[1]
                 if recipient in self.connected_clients:
-                    recipient_writer = self.connected_clients[
-                        recipient
-                    ].get('writer')
-                    recipient_writer.write(
-                        f'Private!{user_nickname}: '
-                        f'{private_message}\n'.encode()
-                    )
-                    writer.write(
-                        f'Server!Private message was '
-                        f'sent to {recipient}\n'.encode()
-                    )
-                    await recipient_writer.drain()
+                    self.connected_clients[recipient]['claims'].append(user_nickname)
+                    writer.write(f'Server!User {recipient} claimed by {user_nickname}\n'.encode())
+                    await writer.drain()
                 else:
-                    writer.write(
-                        f'Server!User {recipient} '
-                        f'is not connected\n'.encode()
-                    )
+                    writer.write(f'Server!User {recipient} is not connected\n'.encode())
                     await writer.drain()
             else:
-                writer.write(
-                    'Server!Don\'t use @ symbol if its not a command!\n'.encode()
-                )
+                writer.write('Server!Don\'t use @ symbol if its not a command!\n'.encode())
                 await writer.drain()
+        else:
+            await self.send_private_message(message, user_nickname, writer)
 
+    async def send_private_message(
+        self,
+        message: str,
+        user_nickname: str,
+        writer: asyncio.StreamWriter,
+    ) -> None:
+        """
+        Отслыкает пользователю приватное сообщение.
+        """
+        tokens = message[1:].split(' ', 1)
+        if len(tokens) == 2:
+            recipient, private_message = tokens
+            if recipient in self.connected_clients:
+                recipient_writer = self.connected_clients[recipient].get('writer')
+                recipient_writer.write(
+                    f'Private!{user_nickname}: {private_message}\n'.encode()
+                )
+                writer.write(f'Server!Private message was sent to {recipient}\n'.encode())
+                await recipient_writer.drain()
+            else:
+                writer.write(f'Server!User {recipient} is not connected\n'.encode())
+                await writer.drain()
+        else:
+            writer.write('Server!Don\'t use @ symbol if its not a command!\n'.encode())
+            await writer.drain()
+
+    async def send_help_message(self, writer: asyncio.StreamWriter) -> None:
+        """
+        Отсылает пользователю сообщение справки.
+        """
+        writer.write(self.help_message.encode())
+        await writer.drain()
 
     async def broadcast_message(self, message: str) -> None:
         """
@@ -161,8 +176,7 @@ class Server:
         """
         Запуск сервера.
         """
-        srv = await asyncio.start_server(
-            self.client_connected, self.host, self.port)
+        srv = await asyncio.start_server(self.client_connected, self.host, self.port)
         async with srv:
             logger.info('Server started on %s:%s', self.host, self.port)
             tasks = [
@@ -172,7 +186,7 @@ class Server:
             await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    server = Server(host='127.0.0.1', port=8000, max_chat_messages=3)
+    server = Server(host='127.0.0.1', port=8000, max_chat_messages=10)
     asyncio.run(server.listen())
 
 # Регистрация клиента (поле юзеров)
